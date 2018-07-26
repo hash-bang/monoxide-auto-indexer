0eb3d8f01d8cda337695a585d5c878f9ced7139a
Matt Carter <m@ttcarter.com>
2018-07-26 17:13:35 +0100

    Switched to async structure

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var async = require('async-chainable');
 
 /**
 * Factory function to create a autoIndexer instance populated with settings
@@ -14,25 +15,52 @@ module.exports = function(options) {
 	return function(finish, monoxide) {
 		_(monoxide.models)
 			.forEach(model => model.hook('query', (done, q) => {
-				var indexes = [];
+				async()
+					// Determine indexes {{{
+					.then('indexes', function(next) {
+						var indexes = [];
 
-				// Query fields {{{
-				var fields = _(q)
-					.pickBy((criteria, field) => !field.startsWith('$'))
-					.keys()
-					.value();
+						// Query fields {{{
+						var fields = _(q)
+							.pickBy((criteria, field) => !field.startsWith('$'))
+							.keys()
+							.value();
 
-				if (fields.length) indexes = indexes.concat(fields);
-				// }}}
-				// Sort {{{
-				if (q.$sort) indexes = indexes.concat(q.$sort);
-				// }}}
-				// Sort all keys {{{
-				indexes.sort();
-				// }}}
+						if (fields.length) indexes = indexes.concat(fields);
+						// }}}
+						// Sort {{{
+						if (q.$sort) indexes = indexes.concat(q.$sort);
+						// }}}
+						// Sort all keys {{{
+						indexes.sort();
+						// }}}
 
-				model.fire('autoIndexer.query', null, indexes);
-				done();
+						next(null, indexes);
+					})
+					// }}}
+					// Fire event autoIndexer.query {{{
+					.then(function(next) {
+						model.fire('autoIndexer.query', undefined, this.indexes);
+					})
+					// }}}
+					// Scoop existing indexes (with a throttle) {{{
+					.then('existingIndexes', function(next) {
+						model.getIndexes(next);
+					})
+					// }}}
+					// Create the index {{{
+					.then(function(next) {
+						// model.$mongoModel.createIndex
+						console.log('BUILD',
+							_(this.indexes)
+								.mapKeys()
+								.mapValues(k => k.startsWith('-') ? -1 : 1)
+								.mapKeys((v, k) => _.trimStart(k, '-'))
+								.value()
+						);
+					})
+					// }}}
+					.end(done);
 			}));
 
 		return finish();
